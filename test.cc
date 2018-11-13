d3bd061558882b228eb51173e61259e1e477516c
emmaestet <emmaestet@gmail.com>
2018-11-13 15:16:54 +0100

    test6

--- test.cc
+++ test.cc
@@ -1,14 +1,10 @@
 #include "test.h"
 #include "ev3dev.h"
-#include <iostream>
 
 void Test::run() const {
-  typedef std::string         mode_type;
-  typedef std::set<mode_type> mode_set;
-  typedef std::string         address_type;
 
   ev3dev::large_motor A(ev3dev::OUTPUT_A);
-
+  A.set_speed_sp(A.max_speed());
   A.run_forever();
 
 }
