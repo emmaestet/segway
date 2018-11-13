605f53b43ef55d047ede673168c4992f9adb8a0f
emmaestet <emmaestet@gmail.com>
2018-11-13 15:54:13 +0100

    test10

--- test.cc
+++ test.cc
@@ -4,7 +4,9 @@
 int Test::run() const {
 
   ev3dev::large_motor A(ev3dev::OUTPUT_A);
+
   if(A.connected()){
+    A.set_command("running");
     A.set_speed_sp(A.max_speed());
     A.run_forever();
     return 0;
