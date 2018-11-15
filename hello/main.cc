5b26d1c95be504bbf03d59caf76113fbd39fd984
emmaestet <emmaestet@gmail.com>
2018-11-15 12:08:11 +0100

    testp1

--- hello/main.cc
+++
@@ -1,8 +0,0 @@
-#include "test.h"
-
-
-int main() {
-  Test m;
-  int r = m.run();
-  return r;
-}
--- hello/script.sh
+++
@@ -1,9 +0,0 @@
-  #!/bin/bash
-
-  make && make clean
-
-  echo "compiled"
-
-  scp -fr /path/to/target robot@ev3:/path/to/destination
-
-  echo "finished"
